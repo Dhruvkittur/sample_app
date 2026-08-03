33e293444342ba10dd962d20bc02cd6221dd7fc2
Dhruvkittur <145898159+Dhruvkittur@users.noreply.github.com>
2026-08-03 10:56:28 +0530

    Implement age input and eligibility message
    Add age input and voting eligibility check.

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ st.title("MY FIRST STEAMLIT APP")
 name=st.text_input("Enter your Name")
 if st.button("Submit"):
    st.write(f"Hello,{name}")
+
+
+age=st.int_input("Enter your age")
+if st.button("Submit"):
+   if age>=18:
+      st.print("Eligible to vote")
+   else:
+      st.write("Sorry not eligible") 
+   
+
+   
